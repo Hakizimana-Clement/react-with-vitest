25a235ec5babad00d049eba1409ebad46da5fce9
Hakizimana Clement <h.clementoert@gmail.com>
2024-06-07 05:43:04 +0200

    correct how file arrangaged

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+function App() {
+  return (
+    <>
+      <h1>Vitest test</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat rerum
+        cupiditate cumque sequi non magnam aspernatur voluptas quasi inventore
+        doloremque ad blanditiis ab adipisci quidem veritatis voluptates ipsa,
+        porro iure?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, quam.
+        Aspernatur consequuntur quos animi libero magnam iste magni iure ratione
+        blanditiis sequi exercitationem quia voluptas mollitia numquam aliquam,
+        ipsum autem. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Repellat rerum cupiditate cumque sequi non magnam aspernatur voluptas
+        quasi inventore doloremque ad blanditiis ab adipisci quidem veritatis
+        voluptates ipsa, porro iure?
+      </p>
+
+      <h2>coverage</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, ducimus
+        commodi consectetur maiores ullam voluptas quaerat blanditiis maxime
+        ipsam in? Architecto nihil animi explicabo voluptatum minus, quia
+        repellat placeat cum?
+      </p>
+
+      <h2>code climate</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        illo, iure necessitatibus consequuntur exercitationem blanditiis
+        inventore ducimus? Voluptatum provident animi dolor ea temporibus,
+        accusamus facere necessitatibus, quo beatae sed veritatis! Lorem, ipsum
+        dolor sit amet consectetur adipisicing elit. Ut dicta repellat quia
+        assumenda. Tempora hic sequi quasi autem, quod nam fugit at vero in
+        aspernatur dolorem praesentium repudiandae maiores numquam? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Harum nostrum hic fugit,
+        soluta aut quae molestias quia reprehenderit sint sunt quibusdam
+        repellendus aspernatur, vel deserunt deleniti amet molestiae quidem!
+        Quia!
+      </p>
+    </>
+  );
+}
+
+export default App;
